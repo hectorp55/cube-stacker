5a2e5af1784c1957b390df6c906465c8a8aacbb6
hectorp55 <jhgpuga@gmail.com>
2025-08-17 17:20:25 -0500

    Adding comments to constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,11 +4,14 @@ public class Constants
 {
     // Position distance between block locations
     public const float STEP_SIZE = 1.5f;
+    // Starting from the middle how many steps untill we hit the edge of the screen
     public const float STEPS_TO_EDGE = 5;
-
+    // Middle starting position
     public const int STARTING_BLOCK_POSITION = 0;
-    public const int RIGHT_EDGE_BLOCK_POSITION = 5;
+    // Position of block when it should turn around on edge
+    public const int RIGHT_EDGE_BLOCK_POSITION = STEPS_TO_EDGE;
+    // Position of block when it should turn around on edge
     public const int LEFT_EDGE_BLOCK_POSITION = RIGHT_EDGE_BLOCK_POSITION * -1;
-
+    // Middle transform.position.x for a block
     public const float STARTING_X_POSITION = 0; 
 }
