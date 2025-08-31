368070eb83b65b690539d9624a57bcbc8a4ef210
hectorp55 <jhgpuga@gmail.com>
2025-08-31 17:09:40 -0500

    Adjusting starting bg icon locations

--- Assets/Scripts/UI/MainGame/BackgroundImagePlacer.cs
+++ Assets/Scripts/UI/MainGame/BackgroundImagePlacer.cs
@@ -4,6 +4,7 @@ public class BackgroundImagePlacer : MonoBehaviour
 {
     public int PositionLevel;
     private float bottomOfScreen = -10.5f;
+    private int levelCameraStartsMoving = 9;
 
     // ===========================================================
     // Mono Methods
@@ -19,9 +20,9 @@ public class BackgroundImagePlacer : MonoBehaviour
     // ===========================================================
 
     private Vector3 getStartingPosition() {
-        // starting position - (step to get to you + half step)
+        // starting position - (step to get to you - starting steps + half step)
         float startingHeight = Constants.STEP_SIZE * PositionLevel + bottomOfScreen;
-        float assignedHeight = startingHeight - (PositionLevel * Constants.STEP_SIZE / 2);
+        float assignedHeight = startingHeight - ((PositionLevel - levelCameraStartsMoving) * Constants.STEP_SIZE / 2);
         return new Vector3(transform.position.x, assignedHeight, transform.position.z);
     }
 }
