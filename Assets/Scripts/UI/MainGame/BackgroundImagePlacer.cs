f2ceb109b16eb25dee0a4e1df8679379f0091c54
hectorp55 <jhgpuga@gmail.com>
2025-09-01 17:35:43 -0500

    Fixed the height calculations

--- Assets/Scripts/UI/MainGame/BackgroundImagePlacer.cs
+++ Assets/Scripts/UI/MainGame/BackgroundImagePlacer.cs
@@ -22,7 +22,7 @@ public class BackgroundImagePlacer : MonoBehaviour
     private Vector3 getStartingPosition() {
         // starting position - (step to get to you - starting steps + half step)
         float startingHeight = Constants.STEP_SIZE * PositionLevel + bottomOfScreen;
-        float assignedHeight = startingHeight - ((PositionLevel - levelCameraStartsMoving) * Constants.STEP_SIZE / 2);
+        float assignedHeight = startingHeight - (PositionLevel * (Constants.STEP_SIZE / 2));
         return new Vector3(transform.position.x, assignedHeight, transform.position.z);
     }
 }
